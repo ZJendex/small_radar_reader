20f6ae95aa17fc44be336a5277fcc4f9cfe9fd80
ZJendex <zhubean.s@gmail.com>
2025-05-23 17:29:00 -0400

    Target phase seems working -- need more accurate red dot + filtering

--- realtime/realtime_radar_streaming_2D_TargetingPhase.py
+++ realtime/realtime_radar_streaming_2D_TargetingPhase.py
@@ -13,7 +13,9 @@ import organizer_realtime as org
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from utils.range_spectrum import compute_range_spectrum_onChirp
-from utils.range_spectrum import construct_polar_space, compute_rangeFFT_BF
+from utils.range_spectrum import construct_polar_space, compute_rangeFFT_BF, get_phase
+from collections import deque
+
 
 
 import pickle
@@ -32,7 +34,7 @@ interval        = 300 / fps  # update interval [ms] smaller then 1000/fps will b
 
 # ——— User CONFIG ———
 limit_meter     = 1           # range limit [m]
-data_tofu       = 4          # decide to loss how many frame for streaming stablization [a sweet spot - not recommand to change]
+data_tofu       = 8          # decide to loss how many frame for streaming stablization [4 is the sweet spot - not recommand to change]
 save_recording  = False      # save frames to file
 
 # ——— Real fps Compute CONFIG ———
@@ -141,7 +143,6 @@ def on_key_press(event):
             position[0] = position_x_limit[1]
     else:
         return  
-    print(f"Position: {position}")
 
 if __name__ == "__main__":
     
@@ -150,36 +151,50 @@ if __name__ == "__main__":
     t.start()
     
     listener = PacketListenerThread(daemon=True)
+
+    # Set up the plot
+    az_idx, rng_idx = position[0], position[1]
+    fps_fix = config.fps / data_tofu
+    window_sec = 3
+    max_len = int(fps_fix * window_sec)
+    displacement_history = deque(maxlen=max_len)
+    time_history = deque(maxlen=max_len)
+    phase_history = []
+
     range_limit = int(limit_meter/0.0422) # 5m
-
     x_axis, y_axis = construct_polar_space()
-    # init
     Z0 = np.zeros((x_axis.shape[0], range_limit))
-
-    fig, ax = plt.subplots(figsize=(14, 8))
-    mesh = ax.pcolormesh(
-        y_axis[:, :range_limit],
-        x_axis[:, :range_limit],
-        Z0,
-    )
-    cbar = fig.colorbar(mesh)
-    ax.set_xlabel('X-Axis (meters)')
-    ax.set_ylabel('Y-Axis (meters)')
-    ax.set_ylim([0, limit_meter])
-    dot, = ax.plot(position[0], position[1], 'ro', markersize=8) 
-    fps_text = ax.text(0.01, 0.99, '', transform=ax.transAxes, va='top')
+    
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 16))
+    # subplot 1：2D radar plot + red dot targeting
+    mesh = ax1.pcolormesh(y_axis[:, :range_limit], x_axis[:, :range_limit], Z0)
+    dot, = ax1.plot([], [], 'ro', markersize=8)
+    fps_text = ax1.text(0.01, 0.99, '', transform=ax1.transAxes, va='top')
+    ax1.set_xlabel('X-Axis (m)')
+    ax1.set_ylabel('Y-Axis (m)')
+    ax1.set_ylim([0, limit_meter])
     mesh.set_clim(0, 350000)
+
+    # subplot 2：phase at red dot
+    line, = ax2.plot([], [], '-')
+    ax2.set_xlim(0, window_sec)
+    ax2.set_xlabel('Time (s)')
+    ax2.set_ylabel('Displacement (mm)')
+    ax2.set_title(f'Displacement at Bin (az:{position[0]}, rng:{position[1]})')
+    ax2.grid(True)
 
     def init():
         mesh.set_array(Z0.ravel())
-        return mesh,
+        line.set_data([], [])
+        dot.set_data([], [])
+        return mesh, dot, line
 
     def update(_):
-        global last_time, frame_count, real_fps, position, position_x_limit
+        global last_time, frame_count, real_fps, position, az_idx, rng_idx, phase_history
         try:
             frame = frame_queue.get_nowait()
         except queue.Empty:
-            return (mesh,)
+            return mesh, dot, line
         
         if save_recording:
             realtime_frames.append(frame)
@@ -196,23 +211,46 @@ if __name__ == "__main__":
             last_time = current_time
             frame_count = 0
 
+        # --- 2D fft ---
         fft_data = compute_rangeFFT_BF(frame)
-        fft_data = np.abs(fft_data)
-        Z = fft_data[:, :range_limit]
+        # --- update pcolormesh ---
+        Spec_2D = np.abs(fft_data)
+        Z = Spec_2D[:, :range_limit]
         mesh.set_array(Z.ravel())
-
         # Update the position of the dot
         x_dot = x_axis[position[0],position[1]]
         y_dot = y_axis[position[0],position[1]]
         if position[0] < 0:
                 y_dot = -y_dot
                 x_dot = -x_dot
-        print(f"X: {x_dot}, Y: {y_dot}")
         dot.set_data([x_dot],
              [y_dot])
         # vmin, vmax = Z.min(), Z.max()
         # mesh.set_clim(vmin, vmax)
-        return mesh,
+
+        # --- update phase plot ---
+        if az_idx != position[0] or rng_idx != position[1]:
+            displacement_history.clear()
+            time_history.clear()
+            az_idx, rng_idx = position[0], position[1]
+        disp = get_phase(fft_data[position[0], position[1]])
+        t = current_time
+        time_history.append(t)
+        displacement_history.append(disp)
+        t0 = time_history[0]
+        time_shifted = [ti - t0 for ti in time_history]
+        # unwrap and detrend the phase when have enough data
+        if len(time_shifted) > max_len/2:
+            phase_history = np.unwrap(displacement_history).tolist()
+        else:
+            phase_history = displacement_history
+        # update the line plot
+        line.set_data(time_shifted, phase_history)
+        ax2.set_xlim(0, window_sec)
+        ax2.relim()            # recalculate data limits
+        ax2.autoscale_view()
+        ax2.set_title(f'Displacement at Bin (az:{position[0]}, rng:{position[1]})')
+        return mesh, dot, line
 
     try:
         listener.start()
